1eacde507eca88be9574c9b5d79b593d1191b73b
itscaiocunha <caio.cunha@sou.unifeob.edu.br>
2025-02-26 15:46:49 -0300

    feat: cors update

--- app/api/produtos/route.js
+++ app/api/produtos/route.js
@@ -13,13 +13,26 @@ export async function GET(req) {
   return await getAllProducts();
 }
 
+// Função para incluir cabeçalhos CORS nas respostas
+function createResponse(data, status = 200) {
+  return new Response(JSON.stringify(data), {
+    status,
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*', // Permite requisições de qualquer origem
+      'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+      'Access-Control-Allow-Headers': 'Content-Type',
+    },
+  });
+}
+
 async function getAllProducts() {
   try {
     const produtos = await prisma.produtos.findMany();
-    return new Response(JSON.stringify(produtos), { status: 200 });
+    return createResponse(produtos);
   } catch (error) {
     console.error('Erro ao buscar todos os produtos:', error);
-    return new Response(JSON.stringify({ message: 'Erro ao buscar produtos' }), { status: 500 });
+    return createResponse({ message: 'Erro ao buscar produtos' }, 500);
   }
 }
 
@@ -27,18 +40,16 @@ async function getProductById(id) {
   try {
     const produto = await prisma.produtos.findUnique({
       where: { id },
-      include: {
-        categorias: true
-      }
+      include: { categorias: true },
     });
 
     if (!produto) {
-      return new Response(JSON.stringify({ message: 'Produto não encontrado' }), { status: 404 });
+      return createResponse({ message: 'Produto não encontrado' }, 404);
     }
 
-    return new Response(JSON.stringify(produto), { status: 200 });
+    return createResponse(produto);
   } catch (error) {
     console.error('Erro ao buscar produto:', error);
-    return new Response(JSON.stringify({ message: 'Erro ao buscar produto' }), { status: 500 });
+    return createResponse({ message: 'Erro ao buscar produto' }, 500);
   }
 }
