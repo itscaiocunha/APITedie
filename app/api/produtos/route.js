ca543f37aa84babe68206dddb598db8c2c61743e
itscaiocunha <caio.cunha@sou.unifeob.edu.br>
2025-02-26 16:14:56 -0300

    feat: update

--- app/api/produtos/route.js
+++ app/api/produtos/route.js
@@ -3,53 +3,60 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 export async function GET(req) {
-  const { searchParams } = new URL(req.url);
-  const id = searchParams.get('id');
+  try {
+    const { searchParams } = new URL(req.url);
+    const id = searchParams.get('id');
 
-  if (id) {
-    return await getProductById(parseInt(id));
+    if (id) {
+      const productId = Number(id);
+      if (isNaN(productId)) {
+        return createResponse({ message: 'ID inválido' }, 400);
+      }
+      return await fetchProductById(productId);
+    }
+
+    return await fetchAllProducts();
+  } catch (error) {
+    console.error('Erro na requisição:', error);
+    return createResponse({ message: 'Erro interno no servidor' }, 500);
   }
-
-  return await getAllProducts();
 }
 
-// Função para incluir cabeçalhos CORS nas respostas
+async function fetchAllProducts() {
+  try {
+    const products = await prisma.produtos.findMany();
+    return createResponse(products);
+  } catch (error) {
+    console.error('Erro ao buscar produtos:', error);
+    return createResponse({ message: 'Erro ao buscar produtos' }, 500);
+  }
+}
+
+async function fetchProductById(id) {
+  try {
+    const product = await prisma.produtos.findUnique({
+      where: { id },
+    });
+
+    if (!product) {
+      return createResponse({ message: 'Produto não encontrado' }, 404);
+    }
+
+    return createResponse(product);
+  } catch (error) {
+    console.error('Erro ao buscar produto:', error);
+    return createResponse({ message: 'Erro ao buscar produto' }, 500);
+  }
+}
+
 function createResponse(data, status = 200) {
   return new Response(JSON.stringify(data), {
     status,
     headers: {
       'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': '*', // Permite requisições de qualquer origem
+      'Access-Control-Allow-Origin': '*',
       'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
       'Access-Control-Allow-Headers': 'Content-Type',
     },
   });
-}
-
-async function getAllProducts() {
-  try {
-    const produtos = await prisma.produtos.findMany();
-    return createResponse(produtos);
-  } catch (error) {
-    console.error('Erro ao buscar todos os produtos:', error);
-    return createResponse({ message: 'Erro ao buscar produtos' }, 500);
-  }
-}
-
-async function getProductById(id) {
-  try {
-    const produto = await prisma.produtos.findUnique({
-      where: { id },
-      include: { categorias: true },
-    });
-
-    if (!produto) {
-      return createResponse({ message: 'Produto não encontrado' }, 404);
-    }
-
-    return createResponse(produto);
-  } catch (error) {
-    console.error('Erro ao buscar produto:', error);
-    return createResponse({ message: 'Erro ao buscar produto' }, 500);
-  }
 }
